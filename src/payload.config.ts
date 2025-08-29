4348867f6354fc8475a219bd8da1e8f54a215f1a
Hamza <144457566+unk-user@users.noreply.github.com>
2025-08-29 21:53:37 +0100

    add payload cms

---
+++ src/payload.config.ts
@@ -0,0 +1,30 @@
+// storage-adapter-import-placeholder
+import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
+import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+import { fileURLToPath } from "url";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  collections: [],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || "",
+  typescript: {
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+  db: vercelPostgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URL || "",
+    },
+  }),
+  sharp,
+  plugins: [
+    payloadCloudPlugin(),
+    // storage-adapter-placeholder
+  ],
+});
